51bb83a53e35955674ba7f4632a57e0cc0d3abfa
Murillo <murillons11@gmail.com>
2023-03-13 10:11:53 -0300

    Modificado o index e o array

--- prog_array.js
+++ prog_array.js
@@ -4,11 +4,16 @@ let nome_cidade = []
 
 function inserir(){
     let cidade = document.getElementById("cidade").value
-    console.log(cidade)
     nome_cidade.push(cidade)
     console.log(nome_cidade)
 }
 
+
+function excluir(){
+    let cidade = document.getElementById("cidade").value
+    nome_cidade.pop(cidade)
+    console.log(nome_cidade)
+}
 
 
 

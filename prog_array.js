78ee414d68c284a211daaff9b647260ce4af8e9f
Murillo <murillons11@gmail.com>
2023-03-13 08:22:16 -0300

    Modificado css e html e js

---
+++ prog_array.js
@@ -0,0 +1,19 @@
+let res = document.getElementById("res")
+let nome_cidade = []
+
+
+function inserir(){
+    let cidade = document.getElementById("cidade").value
+    console.log(cidade)
+    nome_cidade.push(cidade)
+    console.log(nome_cidade)
+}
+
+
+
+
+
+
+
+
+
